1ce4b0b5aadba0ffd07be9b58405b488ef31c4c7
Kostiantyn <kostiaolnev@gmail.com>
2026-09-01 05:26:35 -0700

    add dynamic loading test

---
+++ tests/test_dynamic_loading.py
@@ -0,0 +1,27 @@
+import pytest
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.webdriver import WebDriver
+from pages.dynamic_loading_page import DynamicLoadingPage
+
+from locators.dynamic_loading_locators import START_BUTTON, FINISH_LOCATOR
+
+class TestDynamicLoading():
+    @pytest.fixture(autouse=True)
+    def setup(self, driver: WebDriver):
+        self.driver = driver
+
+    URL = "https://the-internet.herokuapp.com/dynamic_loading/1"
+    TEXT_HELLO = "Hello World!"
+
+    def test_dynamic_loading(self):
+        dynamic_page = DynamicLoadingPage(self.driver)
+
+        dynamic_page.open(self.URL)
+        dynamic_page.loaging_click(START_BUTTON)
+        text = dynamic_page.get_hello_world_text(FINISH_LOCATOR)
+        
+        assert self.TEXT_HELLO in text
+
+        
