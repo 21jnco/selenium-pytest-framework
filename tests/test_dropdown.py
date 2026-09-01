9f6aabee0cf86a5ff3d3c3bc597f5da6182acaab
Kostiantyn <kostiaolnev@gmail.com>
2026-09-01 05:25:18 -0700

    update test dropdown

--- tests/test_dropdown.py
+++ tests/test_dropdown.py
@@ -1,24 +1,24 @@
 import pytest
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.support.ui import Select
+from pages.dropdown_page import DropdownPage
 from locators.dropdown_locators import DROPDOWN_BUTTON
-
+from pages.dropdown_page import DropdownPage
 
 class TestDropdown():
     @pytest.fixture(autouse=True)
     def setup(self, driver: WebDriver):
         self.driver = driver
 
+    OPTION = "Option 1"
     URL = "https://the-internet.herokuapp.com/dropdown"
-    OPTION = "Option 2"
 
     def test_dropdown(self):
-        self.driver.get(self.URL)
-        element = self.driver.find_element(*DROPDOWN_BUTTON)
-        dropdown = Select(element)
-        dropdown.select_by_value("2")
-        selected_text = dropdown.first_selected_option.text
+        dropdown_page = DropdownPage(self.driver)
 
-        assert self.OPTION in selected_text
+        dropdown_page.open(self.URL)
+        element = dropdown_page.get_element(DROPDOWN_BUTTON)
+        dropdown = dropdown_page.select_dropdown(element, self.OPTION)
+        text = dropdown_page.get_dropdown_text(dropdown)
+
+        assert self.OPTION in text
         
